8c150d9df35aaaf765894c67063c1fcdafd77489
Yuta Tamura <tamura.go.tamura@gmail.com>
2020-04-06 18:37:30 +0900

    shceduleの工作

--- schedule/looper.py
+++ schedule/looper.py
@@ -2,18 +2,15 @@ import requests
 import json
 import schedule
 
-def job:():
-    #POSTするURL設定
-    post_url = "https://app.takopanman.work/post/"
+#POSTするURL設定
+post_url = "https://app.takopanman.work/post/"
 
-    #POSTするファイルの読込
-    files = { "image_file": open('./reset_data.jpeg', 'rb') }
+#POSTするファイルの読込
+files = { "image_file": open('./reset_data.jpeg', 'rb') }
 
-    #POST送信
-    response = requests.post(
-        post_url,
-        files = files,)
+#POST送信
+response = requests.post(
+    post_url,
+    files = files,)
 
-    print(json.loads(response.text))
-
-schedule
+print(json.loads(response.text))
