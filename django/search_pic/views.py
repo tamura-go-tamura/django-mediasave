e8ba8deb483394ac9ed7b4f6702193589d31b26b
Yuta Tamura <tamura.go.tamura@gmail.com>
2020-04-07 11:05:47 +0900

    adminの追加

--- django/search_pic/views.py
+++ django/search_pic/views.py
@@ -15,7 +15,7 @@ def index(request):
                  Q(file_name__icontains=keyword)
                )
         messages.success(request, '「{}」の検索結果'.format(keyword))
-    return render(request, 'index.html', {'files': files })
+    return render(request, 'index.html', {'files': files, 'admin': False  })
 
 def auth(request):
     pic = request.GET.get('pic')
@@ -30,6 +30,11 @@ def show(request):
         pwd = None
         return redirect(request.build_absolute_uri()[:-6])
 
+    if pic == "admin" and pwd == "adminPassword":
+        messages.error(request, '全ての画像を表示します')
+        files = FILENAME.objects.all()
+        return render(request, 'index.html', {'files': files, 'admin': True })
+
     try:
         selected_file = FILENAME.objects.get(pwd = pwd)
         if pic == selected_file.file_name:
@@ -37,9 +42,9 @@ def show(request):
         else:
             messages.error(request, 'パスワードが間違っています')
             files = None
-            return render(request, 'index.html', {'files': files })
+            return render(request, 'index.html', {'files': files, 'admin': False })
     except:
         messages.error(request, 'パスワードが間違っています')
         files = None
-        return render(request, 'index.html', {'files': files })
+        return render(request, 'index.html', {'files': files, 'admin': False  })
         
